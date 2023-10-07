2e948bb00d4c4f4df911006dd1bc4f755ed82f23
Dirlay <tertiaryperson@gmail.com>
2023-10-07 11:59:25 +0200

    Create Branch to test GIF animations - included AnimatedGIF library - first steps implementing a breath animation gif

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include "pin_config.h"
 #include "img_logo.h"
 
+#include "AnimatedGIF.h"
+#include "breath.gif"
+
 #define TFT_CHANNEL 0
 #define TFT_FREQUENCY 2000
 #define TFT_RESOLUTION 8
@@ -24,6 +27,7 @@ TFT_eSprite ppmtext = TFT_eSprite(&tft);
 
 uint8_t textWidth = 5, textHeight = 7, textColon = 2;
 int showNumbers;
+int breathFrame = 0;
 
 SCD30_I2C sensorCO2;
 
@@ -36,10 +40,13 @@ RTC_DS3231 rtc;
 DateTime now;
 char daysOfTheWeek[7][3] = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
 
+AnimatedGIF gif;
+
 unsigned long currentMillis = 0, previousMillis = 0; // timer variable
 unsigned long timerCO2 = 0, timerRTC = 0;
 
 void displaySprite();
+void GIFDraw();
 
 void setup()
 {
@@ -95,7 +102,9 @@ void setup()
         rtc.disable32K();
         tft.println("READY");
     }
-    // delay(1000);
+    delay(1000);
+    
+    gif.begin(GIF_PALETTE_RGB565_BE); // big endian pixels
 }
 
 void loop()
@@ -123,9 +132,18 @@ void loop()
 void displaySprite()
 {
     background.createSprite(320, 170);
-    background.setSwapBytes(true);
-    background.pushImage(0, 0, 320, 170, (uint16_t *)img_logo);
-    background.setSwapBytes(false);
+    // background.setSwapBytes(true);
+    breathFrame < 150 ? breathFrame++ : breathFrame = 0;
+    if (gif.open("/breath.gif", GIFOpenFile, GIFCloseFile, GIFReadFile, GIFSeekFile, GIFDraw)) {
+        tft.startWrite();
+        gif.playFrame(false, NULL);
+        gif.close();
+        tft.endWrite();
+    }
+    else [
+        Serial.println("GIF ERROR!");
+    ]
+    // background.setSwapBytes(false);
 
     fps.createSprite(2 * 14, 19);
     fps.fillSprite(TFT_MAGENTA);
@@ -180,3 +198,114 @@ void displaySprite()
     background.pushSprite(0, 0, TFT_MAGENTA);
     background.deleteSprite();
 }
+
+void GIFDraw(GIFDRAW *pDraw)
+{
+    uint8_t *s;
+    uint16_t *d, *usPalette, usTemp[320];
+    int x, iWidth;
+    int startx, starty;
+    int bRotate;
+    PRIVATE *pPriv = (PRIVATE *)pDraw->pUser;
+
+    switch (pPriv->angle) {
+      case 0:
+        bRotate = 0;
+        startx = pPriv->xoff + pDraw->iX;
+        starty = pPriv->yoff + pDraw->iY + pDraw->y;
+      break;
+      case 1: // 90
+        bRotate = 1;
+        startx = pPriv->xoff + pDraw->iX + pDraw->y;
+        starty = pPriv->yoff + pDraw->iY;
+      break;
+      case 2: // 180
+        bRotate = 0;
+        startx = pPriv->xoff + pDraw->iX;
+        starty = pPriv->yoff + pDraw->iHeight - pDraw->y;
+      break;
+      case 3: // 270
+        bRotate = 1;
+        startx = pPriv->xoff + pDraw->iX + (pDraw->iHeight - pDraw->y);
+        starty = pPriv->yoff + pDraw->iY;
+      break;
+    }
+    usPalette = pDraw->pPalette;
+    iWidth = pDraw->iWidth;
+    if (iWidth > DISPLAY_WIDTH)
+       iWidth = DISPLAY_WIDTH;
+    s = pDraw->pPixels;
+    if (pDraw->ucDisposalMethod == 2) // restore to background color
+    {
+      for (x=0; x<iWidth; x++)
+      {
+        if (s[x] == pDraw->ucTransparent)
+           s[x] = pDraw->ucBackground;
+      }
+      pDraw->ucHasTransparency = 0;
+    }
+    // Apply the new pixels to the main image
+    if (pDraw->ucHasTransparency) // if transparency used
+    {
+      uint8_t *pEnd, c, ucTransparent = pDraw->ucTransparent;
+      int x, iCount;
+      pEnd = s + iWidth;
+      x = 0;
+      iCount = 0; // count non-transparent pixels
+      while(x < iWidth)
+      {
+        c = ucTransparent-1;
+        d = usTemp;
+        while (c != ucTransparent && s < pEnd)
+        {
+          c = *s++;
+          if (c == ucTransparent) // done, stop
+          {
+            s--; // back up to treat it like transparent
+          }
+          else // opaque
+          {
+             *d++ = usPalette[c];
+             iCount++;
+          }
+        } // while looking for opaque pixels
+        if (iCount) // any opaque pixels?
+        {
+          if (bRotate)
+            spilcdSetPosition(&lcd, startx, starty + x, 1, iCount, DRAW_TO_LCD);
+          else
+            spilcdSetPosition(&lcd, startx+x, starty, iCount, 1, DRAW_TO_LCD);
+          spilcdWriteDataBlock(&lcd, (uint8_t *)usTemp, iCount*2, DRAW_TO_LCD);
+          x += iCount;
+          iCount = 0;
+        }
+        // no, look for a run of transparent pixels
+        c = ucTransparent;
+        while (c == ucTransparent && s < pEnd)
+        {
+          c = *s++;
+          if (c == ucTransparent)
+             iCount++;
+          else
+             s--;
+        }
+        if (iCount)
+        {
+          x += iCount; // skip these
+          iCount = 0;
+        }
+      }
+    }
+    else
+    {
+      s = pDraw->pPixels;
+      // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
+      for (x=0; x<iWidth; x++)
+        usTemp[x] = usPalette[*s++];
+      if (bRotate)
+        spilcdSetPosition(&lcd, startx, starty, 1, iWidth, DRAW_TO_LCD);
+      else
+        spilcdSetPosition(&lcd, startx, starty, iWidth, 1, DRAW_TO_LCD);
+      spilcdWriteDataBlock(&lcd, (uint8_t *)usTemp, iWidth*2, DRAW_TO_LCD);
+    }
+} /* GIFDraw() */
